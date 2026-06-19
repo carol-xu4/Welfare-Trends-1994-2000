de652a7ae04c6db12cffdba1a89f821ba3ee3936
Carol Xu <xucarol.20@gmail.com>
2026-06-19 17:09:41 -0400

    meow

---
+++ cleaning/cleaning.r
@@ -0,0 +1,6 @@
+## Preliminaries -----------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggthemes, readxl, data.table, gdata)
+
+# Set working directory ----------------------------------------------------
+setwd("C:/Users/CarolXu/OneDrive - Cato Institute/Desktop/Welfare Trends 1994-2000")
